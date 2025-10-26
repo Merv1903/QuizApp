661d7150caffbaeabf2734110b9117e90a45faf3
Merv1903 <merve1995samur@gmail.com>
2025-10-26 10:25:13 +0100

    Aktualisiere Dateien

---
+++ script.js
@@ -0,0 +1,45 @@
+let questions = [
+{
+"question": "JavaScript File Has An Extension of:",
+"answer_1": ".Java",
+"answer_2": ".Js",
+"answer_3": ".javascript",
+"answer_4": ".xml",
+"right_answer": 2,
+
+},
+
+{
+"question": "Name two different ways to call a function.",
+"answer_1": "When the function is stored in the JavaScript Folder",
+"answer_2": "When it is invoked from JavaScript code",
+"answer_3": "When an event occurs (when a user clicks a button)",
+"answer_4": "When you book a hall for a private function",
+"right_answer": 2,3,
+
+},
+
+{
+"question": "Who developed and designed TypeScript?",
+"answer_1": "Microsoft",
+"answer_2": "Amazon",
+"answer_3": "Oracle",
+"answer_4": "Typescript",
+"right_answer": 1,
+
+},
+
+{
+"question": "Evaluate the following: undefined + 2",
+"answer_1": "Type Error",
+"answer_2": "NaN",
+"answer_3": "Undefined",
+"answer_4": "2",
+"right_answer": 2,
+
+},
+
+
+
+
+];
